fd71d448d97813f9fc6b82cd5acadb55fd68f54e
bosun36 <sviridenko@3davinci.ru>
2026-06-06 08:56:30 +0300

    Initial commit: React + Vite + TypeScript + Tailwind + React Router + Axios

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>Жемчужина</div>
+    </BrowserRouter>
+  )
+}
+
+export default App
